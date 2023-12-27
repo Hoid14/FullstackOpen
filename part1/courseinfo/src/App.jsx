d06368b5a28e283646fe955434c937085b2cf16e
Sergio Arias <s-andres140@hotmail.com>
2023-12-27 11:59:38 -0500

    finish exercise 1.2: courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,12 +3,20 @@ const Header = ({courseName}) =>{
     <h1>{courseName}</h1>
   )
 }
-
-const Content = ({part,exerciseNumber} )=>{
+const Part = ({part,exerciseNumber})=>{
   return(
     <p>
       {part} {exerciseNumber}
     </p>
+  )
+}
+const Content = ({part1,exerciseNumber1,part2,exerciseNumber2,part3,exerciseNumber3} )=>{
+  return(
+    <div>
+      <Part part={part1} exerciseNumber={exerciseNumber1}/>
+      <Part part={part2} exerciseNumber={exerciseNumber2}/>
+      <Part part={part3} exerciseNumber={exerciseNumber3}/>
+    </div>
   )
 }
 
@@ -30,9 +38,7 @@ const App = () => {
   return (
     <div>
       <Header courseName={course}/>
-      <Content part={part1} exerciseNumber={exercises1}/>
-      <Content part={part2} exerciseNumber={exercises2} />
-      <Content part={part3} exerciseNumber={exercises3} />
+      <Content part1={part1} exerciseNumber1={exercises1} part2={part2} exerciseNumber2={exercises2} part3={part3} exerciseNumber3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
