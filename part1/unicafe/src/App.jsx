f1bf3dfa38b977c5c5aa3fe75e360d711ae39284
Sergio Arias <s-andres140@hotmail.com>
2024-01-05 10:10:26 -0500

    1.7: unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,20 +12,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleclick = (value, funcionModificadora) =>{
+  const handleClick = (value, funcionModificadora) =>{
     funcionModificadora(value+1)
+  }
+
+  const totalComentarios = () => good+neutral+bad
+  const puntuacionPromedio = () =>{
+    return(
+      (good*1+neutral*0+bad*-1)/(good+neutral+bad)
+    )
+  }
+  const porcentajeComPositivos = () =>{
+    return(
+      (good/totalComentarios())*100
+    )
   }
   return(
     <div>
       <h1>give feedback</h1>
-      <Button handleclick={()=>handleclick(good, setGood)} text='good'/>
-      <Button handleclick={()=>handleclick(neutral, setNeutral)} text='neutral'/>
-      <Button handleclick={()=>handleclick(bad, setBad)} text='bad'/>
+      <Button handleclick={()=>handleClick(good, setGood)} text='good'/>
+      <Button handleclick={()=>handleClick(neutral, setNeutral)} text='neutral'/>
+      <Button handleclick={()=>handleClick(bad, setBad)} text='bad'/>
       <h1>statistics</h1>
 
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalComentarios()}</p>
+      <p>average {puntuacionPromedio()}</p>
+      <p>positive {porcentajeComPositivos()} %</p>
+
+
 
 
     </div>
