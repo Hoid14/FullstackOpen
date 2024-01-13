af1be680ca00f96371a4320242f8db30c18ff26a
Sergio Arias <s-andres140@hotmail.com>
2024-01-13 11:38:22 -0500

    2.2: Course information step7

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,10 +1,19 @@
 import Part from './Part'
 const Content = ({parts}) =>{
+    console.log(parts);
+    const sumExercises = () =>{
+        let sum = 0
+        for (const part of parts){
+            sum += part.exercises
+        }
+        return sum
+    }
     return(
         <>
         {parts.map(part=>(
             <Part key={part.id} part={part}/>
         ))}
+        <h3>total of {sumExercises()} exercises</h3>
         </>
     )
 }
