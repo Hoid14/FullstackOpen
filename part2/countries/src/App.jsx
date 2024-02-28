243deda01d0b5d382424f7cc79e6713fd0542f69
Sergio Arias <s-andres140@hotmail.com>
2024-02-28 09:07:47 -0500

    2.19*: Data for countries, step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 const App = () => {
+  // Nota: no poner los set... en el cuerpo del componente causa renderizados infinitos
   const [value, setValue] = useState('')
   const [paises, setPaises] = useState([]) // Lista de paises con su informacion
+  const [countryToShow, setCountryToshow] =useState(null)
   useEffect( ()=>{
     axios
     .get(
@@ -30,24 +32,30 @@ const App = () => {
     }
     else{
       return paises.filter(pais =>{
-        const currentPais = pais.name.common.toLocaleLowerCase()
-        const pattern = new RegExp(value.toLocaleLowerCase())
+        const currentPais = pais.name.common.toLowerCase()
+        const pattern = new RegExp(value.toLowerCase())
         return pattern.test(currentPais)
       })
     }
   }
+
+  const currentListCountry = getPaisesToShow(); // en todos los renderizados se establece esta constante con una lista de paises
+
   
-  const countryToShow = getPaisesToShow().length === 1
-  ? getPaisesToShow()[0]
-  :{}
+  useEffect(() => {
+    if (currentListCountry.length === 1) {
+      setCountryToshow(currentListCountry[0]); // 
+    }
+  }, [currentListCountry]); // Dependencia: el efecto se ejecuta solo cuando cambia la variable currenListCountry
   
   const handleChange = (event) => {
     const current = event.target.value
+    setCountryToshow(null)
     setValue(current)
   }
 
-  const handleClick = (event) =>{
-    console.log(event)
+  const handleClick = (pais) =>{
+    setCountryToshow(pais)
   }
   return (
     <>
@@ -58,7 +66,7 @@ const App = () => {
           <div>Too many matches, specify another filter</div>
         )}
 
-        {getPaisesToShow().length>1 &&
+        {countryToShow ===null && getPaisesToShow().length>1 &&
         getPaisesToShow().length <= 10 && // Si la lista tiene una longitud de mas de 1 y menor o igual a 10, muestra la lista
 
         getPaisesToShow().map(pais => (
@@ -67,7 +75,7 @@ const App = () => {
           </div>
         ))}
 
-        {getPaisesToShow().length ===1 &&
+        {countryToShow !== null &&
         <>
           <h1>{countryToShow.name.common}</h1>
           <p>capital {countryToShow.capital[0]}</p>
